6617160a2a6f29918ccb42301f1ac5c6e0995632
Pavel Stepanov <impetus@yandex.ru>
2024-03-11 08:40:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/FlavorFragment.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.databinding.FragmentFlavorBinding;
+
+public class FlavorFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pickup, container, false);
+    }
+}
